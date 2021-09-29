16c25d00a1c9c201ef1a5f850460f14be9f67377
Captain Throwback <captainthrowback@hotmail.com>
2021-09-29 02:01:56 +0200

    I01WD: clean up tree and use twrp-common for decrypt
    - Update recovery.fstab and remove twrp.fstab
    - Add event-log-tags for logd
    - Update USB config and add UMS support
    - Delete zip installer
    - Delete other unnecessary files
    
    Change-Id: I370d837b4c9be4735dcd0ffbbd583787548cb73e

--- device.mk
+++ device.mk
@@ -1,9 +1,16 @@
-LOCAL_PATH := device/asus/I01WD
+# A/B support
+AB_OTA_UPDATER := true
 
-# define hardware platform
-PRODUCT_PLATFORM := msmnile
+# A/B updater updatable partitions list. Keep in sync with the partition list
+# with "_a" and "_b" variants in the device. Note that the vendor can add more
+# more partitions to this list for the bootloader and radio.
+AB_OTA_PARTITIONS += \
+    boot \
+    system \
+    vendor \
+    vbmeta \
+    dtbo
 
-#TEST
 # A/B support
 PRODUCT_PACKAGES += \
     otapreopt_script \
@@ -11,9 +18,6 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_engine_sideload \
     update_verifier
-
-PRODUCT_PACKAGES += \
-    bootctrl.$(PRODUCT_PLATFORM).recovery
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -23,11 +27,28 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.0-impl.recovery \
+    bootctrl.$(PRODUCT_PLATFORM).recovery
 
 PRODUCT_COPY_FILES += \
-    $(OUT_DIR)/target/product/$(PRODUCT_DEVICE)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
+    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
 
-# Include ASUS keys for zip signature verification
-PRODUCT_EXTRA_RECOVERY_KEYS += $(LOCAL_PATH)/security/asus
+# qcom decryption
+PRODUCT_PACKAGES_ENG += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Overrides
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=WW_$(PRODUCT_RELEASE_NAME) \
+    TARGET_DEVICE=ZS630KL
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.product=ZS630KL
+
+# Include OEM keys for zip signature verification
+PRODUCT_EXTRA_RECOVERY_KEYS += $(LOCAL_PATH)/security/$(BOARD_VENDOR)
